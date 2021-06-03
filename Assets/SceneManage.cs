b3244449e42a6636e0ce3fdd10869f70eb3d49a8
yabikuhirohumi <e195762@ie.u-ryukyu.ac.jp>
2021-06-03 10:41:52 +0900

    試し

---
+++ Assets/SceneManage.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    int currentSceneNum;
+    bool isContinue;
+    [SerializeField] GameObject TitlePanel;
+    [SerializeField] GameObject GamePanel;
+    [SerializeField] GameObject ConfigPanel;
+    [SerializeField] GameObject MenuPanel;
+    
+    void Start()
+    {
+        LoadTitle();
+    }
+
+    int GetScene(){return currentSceneNum;}
+    
+    public void ChangeScene(int sceneNum)
+    {
+        switch(sceneNum)
+        {
+            case 0:
+                LoadTitle();
+                break;
+            case 1:
+                LoadConfig();
+                break;
+            case 2:
+                LoadGame();
+                break;
+            case 3:
+                LoadMenu();
+                break;
+        }
+    }
+
+    public void LoadTitle()
+    {
+        TitlePanel.SetActive(true);
+        GamePanel.SetActive(false);
+        ConfigPanel.SetActive(false);
+        MenuPanel.SetActive(false);
+    }
+    
+    public void LoadConfig()
+    {
+        TitlePanel.SetActive(false);
+        ConfigPanel.SetActive(true);
+        GamePanel.SetActive(false);
+        MenuPanel.SetActive(false);
+    }
+    
+    public void LoadGame()
+    {
+        TitlePanel.SetActive(false);
+        GamePanel.SetActive(true);
+        ConfigPanel.SetActive(false);
+        MenuPanel.SetActive(false);
+    }
+    
+    public void LoadMenu()
+    {
+        TitlePanel.SetActive(false);
+        ConfigPanel.SetActive(false);
+        GamePanel.SetActive(true);
+        MenuPanel.SetActive(true);
+    }
+
+    
+    
+    
+}
